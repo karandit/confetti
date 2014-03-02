38e100f9b21f38f551ccce91d5d5b490e1e329e0
Karandi Tamas <tkarandi@gmail.com>
2014-03-02 21:15:18 +0100

    add plug-ins core, xml, tests.xml

---
+++ 00_tests/plugins/org.confetti.tests.xml/src/org/confetti/tests/xml/CompatibilityTest.java
@@ -0,0 +1,14 @@
+package org.confetti.tests.xml;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompatibilityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ 10_framework/plugins/org.confetti.core/src/org/confetti/core/IInstitute.java
@@ -0,0 +1,5 @@
+package org.confetti.core;
+
+public interface IInstitute {
+
+}
