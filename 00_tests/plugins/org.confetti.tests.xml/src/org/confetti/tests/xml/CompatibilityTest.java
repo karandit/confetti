5274964ce5b048c0ef5b0bd578829a3de9d95c17
Karandi Tamas <tkarandi@gmail.com>
2014-03-04 23:12:08 +0100

    add first xml compatibility test

--- 00_tests/plugins/org.confetti.tests.xml/src/org/confetti/tests/xml/CompatibilityTest.java
+++ 00_tests/plugins/org.confetti.tests.xml/src/org/confetti/tests/xml/CompatibilityTest.java
@@ -1,14 +1,23 @@
 package org.confetti.tests.xml;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+
+import org.confetti.xml.FAOException;
+import org.confetti.xml.InstituteFAO;
+import org.confetti.xml.core.InstituteXml;
 import org.junit.Test;
 
 public class CompatibilityTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testUnitedKingdom_Hopwood() throws FAOException {
+		File file = new File("examples/United-Kingdom/Hopwood/Hopwood.fet");
+		InstituteXml inst =  new InstituteFAO().importFrom(file);
+		assertEquals("5.16.0", inst.getVersion());
+		assertEquals("Hopwood ESOL", inst.getName());
+		assertEquals("Default comments", inst.getComment());
 	}
 
 }
