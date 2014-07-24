d3c7a0b9394e8be5d2a49e50def43a556d9736c5
bublag <gaabor6@gmail.com>
2014-07-24 22:29:45 +0200

    progress in NewSubjectVerifyWizardPage
    problem with it: table isn't filling up the entire composite

--- 10_framework/plugins/org.confetti.rcp/src/org/confetti/rcp/wizards/NewSubjectVerifyWizardPage.java
+++ 10_framework/plugins/org.confetti.rcp/src/org/confetti/rcp/wizards/NewSubjectVerifyWizardPage.java
@@ -4,15 +4,14 @@ import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 
 public class NewSubjectVerifyWizardPage extends ModelableWizardPage<NewSubjectInputWizardModel> implements IWizardPageNavigatable {
 
-	private Label label;
-	private Composite mResultComposite;
+	//private Label label;
+	//private Composite mResultComposite;
 	private Table table;
 
 	public NewSubjectVerifyWizardPage(NewSubjectInputWizardModel model) {
@@ -23,27 +22,33 @@ public class NewSubjectVerifyWizardPage extends ModelableWizardPage<NewSubjectIn
 	@Override
 	public void createControl(Composite parent) {
 		// label = new Label(parent, SWT.NONE);
-		table = new Table(parent, SWT.BORDER | SWT.V_SCROLL);
+		table = new Table(parent, SWT.MULTI | SWT.BORDER | SWT.V_SCROLL);
+		//TODO: table to fill up the entire parent composite
+		
 		TableColumn names = new TableColumn(table, SWT.NONE);
+		names.setText("name");
+		names.setWidth(20);
 		TableColumn check = new TableColumn(table, SWT.NONE);
+		check.setText("check");
+		check.setWidth(20);
+		
 		//mResultComposite = new Composite(parent, SWT.BORDER | SWT.V_SCROLL);
 		//mResultComposite.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
 		//mResultComposite.setLayout(new GridLayout(2, true));
+		table.setHeaderVisible(true);
 		setControl(table);
 	}
 
 	@Override
 	public void pageShowed() {
 		List<String> subjects = getModel().getSubjects();
-		// for (String string : subjects) {
-		// label.setText(label.getText() + '\n' + string);
-		// }
+		//for (String string : subjects) {
+		//label.setText(label.getText() + '\n' + string);
+		//}
 		for (String s : subjects) {
-			
 			TableItem item = new TableItem(table, SWT.NONE);
 			item.setText(0, s);
-			TableItem ok = new TableItem(table, SWT.NONE);
-			ok.setText(1, "ok");
+			item.setText(1, "ok");
 			
 //			Label nameLabel = new Label(mResultComposite, SWT.NONE);
 //			nameLabel.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, true));
