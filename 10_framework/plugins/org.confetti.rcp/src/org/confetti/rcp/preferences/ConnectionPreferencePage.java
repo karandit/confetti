df2bf3366153ae10f21048c13fcabb19a8b86789
bublag <gaabor6@gmail.com>
2014-10-18 22:58:11 +0200

    Add ConnectionPreferencePage functions
    Can add new connection from the Connection extension point.
    Can save & load connections with the PreferenceStore

---
+++ 10_framework/plugins/org.confetti.rcp/src/org/confetti/rcp/preferences/ConnectionPreferencePage.java
@@ -0,0 +1,195 @@
+package org.confetti.rcp.preferences;
+
+import static org.confetti.rcp.views.AbstractEntityTableView.createColumn;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.confetti.rcp.ConfettiPlugin;
+import org.confetti.rcp.extensions.ConnectionDescr;
+import org.confetti.rcp.extensions.ConnectionFactory;
+import org.confetti.rcp.extensions.ConnectionRegistry;
+import org.confetti.util.Tuple;
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.ListDialog;
+
+/**
+ * @author Gabor Bubla
+ */
+public class ConnectionPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+
+    //--------------------------- constants ----------------------------------------------------------------------------
+    private static final String KEY_CONNECTIONS = "CONNECTIONS";
+    private static final String KEY_TYPE = "TYPE";
+
+    //--------------------------- fields -------------------------------------------------------------------------------
+    private List<ConnectionDescr> extensions = ConnectionRegistry.INSTANCE.getExtensions();
+    private List<Tuple<String, String>> connectionSettings; 
+	
+	@Override
+	public void init(IWorkbench workbench) {
+	    noDefaultAndApplyButton();
+	    setPreferenceStore(ConfettiPlugin.getDefault().getPreferenceStore());
+	    connectionSettings = getConnectionSettings(getPreferenceStore());
+	}
+	
+	@Override
+	protected Control createContents(Composite parent) {
+	    parent.setLayout(new GridLayout(2, false));
+	    
+	    Table table = new Table(parent, SWT.SINGLE | SWT.FULL_SELECTION | SWT.V_SCROLL | SWT.BORDER);
+	    table.setHeaderVisible(true);
+	    createColumn(table, "Connection name", 150);
+	    createColumn(table, "Connection type", 150);
+	    GridDataFactory.fillDefaults().grab(true, true).applyTo(table);
+	    
+	    TableViewer viewer = new TableViewer(table);
+	    viewer.setContentProvider(new ArrayContentProvider());
+	    viewer.setLabelProvider(new ConnectionLabelProvider());
+	    viewer.setInput(connectionSettings);
+	    
+	    Composite buttonsComposite = new Composite(parent, SWT.NONE);
+	    GridData gridData = new GridData();
+	    gridData.verticalAlignment = GridData.BEGINNING;
+	    buttonsComposite.setLayoutData(gridData);
+	    FillLayout fillLayout = new FillLayout(SWT.VERTICAL);
+	    fillLayout.spacing = 10;
+        buttonsComposite.setLayout(fillLayout);
+	    
+	    Button addButton = new Button(buttonsComposite, SWT.NONE);
+	    addButton.setText("Add");
+	    addButton.addSelectionListener(new AddNewConnectionSelectionListener(viewer));
+	    Button editButton = new Button(buttonsComposite, SWT.NONE);
+	    editButton.setText("Edit");
+	    Button removeButton = new Button(buttonsComposite, SWT.NONE);
+	    removeButton.setText("Remove");
+	    
+	    return parent;
+	}
+	
+	private List<Tuple<String, String>> getConnectionSettings(IPreferenceStore preferenceStore) {
+	    List<Tuple<String, String>> connNamesAndTypes = new LinkedList<>();
+	    String connNamesCSV = preferenceStore.getString(KEY_CONNECTIONS);
+	    String[] connNames = connNamesCSV.split(",");
+	    for (String connName : connNames) {
+            if (!connName.isEmpty()) {
+    	        String connType = preferenceStore.getString(connName + "_" + KEY_TYPE);
+                connNamesAndTypes.add(new Tuple<>(connName, connType));
+            }
+        }
+	    return connNamesAndTypes;
+	}
+
+	private class ConnectionLabelProvider extends LabelProvider implements ITableLabelProvider {
+        @Override public Image getColumnImage(Object element, int columnIndex) { return null; }
+        @Override
+        public String getColumnText(Object element, int columnIndex) {
+            Tuple<String, String> tuple = (Tuple<String, String>) element; 
+            switch (columnIndex) {
+                case 0:     return tuple.getFirst();
+                default:    return tuple.getSecond();
+            }
+        }
+	}
+	
+	private class AddNewConnectionSelectionListener extends SelectionAdapter {
+        
+	    private final TableViewer viewer;
+
+        public AddNewConnectionSelectionListener(TableViewer viewer) {
+            this.viewer = viewer;
+        }
+
+        @Override
+        public void widgetSelected(SelectionEvent e) {
+            Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+            
+            // Connection name
+            InputDialog inputDlg = new InputDialog(shell, "Connection name", "Specify a name for the connection", "", new IInputValidator() {
+                @Override
+                public String isValid(String newText) {
+                    if (newText.isEmpty()) {
+                        return "Please enter the connection name";
+                    }
+                    for (Tuple<String, String> tuple : connectionSettings) {
+                        if (tuple.getFirst().equals(newText)) {
+                            return "Connection name already exists";
+                        }
+                    }
+                    return null;
+                }
+            });
+            if (Window.OK != inputDlg.open()) {
+                return;
+            }
+            String connName = inputDlg.getValue();
+            
+            // Connection type
+            ListDialog dlg = new ListDialog(shell);
+            dlg.setContentProvider(new ArrayContentProvider());
+            dlg.setLabelProvider(new LabelProvider());
+            dlg.setTitle("Connection types");
+            dlg.setMessage("Choose an input");
+            dlg.setInput(extensions); 
+            if (Window.OK != dlg.open()) {
+                return;
+            }
+            Object[] selected = dlg.getResult();
+            if (selected == null || selected.length == 0) {
+                return;
+            }
+            ConnectionDescr selectedDescr = (ConnectionDescr) selected[0];
+
+            // Connection details
+            IPreferenceStore prefStore = getPreferenceStore();
+            ConnectionFactory connFactory = selectedDescr.getConnectionFactory();
+            SelectedConnectionDialog dialog = new SelectedConnectionDialog(shell, prefStore, connFactory, connName);
+            int ret = dialog.open();
+            if (ret == Window.OK) {
+                connectionSettings.add(new Tuple<>(connName, selectedDescr.getDbType()));
+                prefStore.setValue(connName + "_" + KEY_TYPE, selectedDescr.getDbType());
+                prefStore.setValue(KEY_CONNECTIONS, transformToCSV(connectionSettings));
+                viewer.refresh();
+            }
+        }
+
+        private String transformToCSV(List<Tuple<String, String>> connectionSettings) {
+            StringBuilder sb = new StringBuilder();
+            boolean first = true;
+            for (Tuple<String, String> tuple : connectionSettings) {
+                if (first) {
+                    first = false;
+                } else {
+                    sb.append(",");
+                }
+                sb.append(tuple.getFirst());
+            }
+            return sb.toString();
+        }
+	}
+	
+}
---
+++ 10_framework/plugins/org.confetti.rcp/src/org/confetti/rcp/preferences/SelectedConnectionDialog.java
@@ -0,0 +1,55 @@
+package org.confetti.rcp.preferences;
+
+import org.confetti.rcp.extensions.ConnectionFactory;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * @author Gabor Bubla
+ */
+class SelectedConnectionDialog extends Dialog {
+
+    private ConnectionFactory connectionFactory;
+    private String connName;
+    private IPreferenceStore prefStore;
+
+    public SelectedConnectionDialog(Shell parentShell, IPreferenceStore prefStore, ConnectionFactory connectionFactory, String connName) {
+        super(parentShell);
+        this.prefStore = prefStore;
+        this.connectionFactory = connectionFactory;
+        this.connName = connName;
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        Composite container = (Composite) super.createDialogArea(parent);
+        connectionFactory.createComposite(container);
+        connectionFactory.load(connName, prefStore);
+        return container;
+    }
+
+    @Override
+    protected void buttonPressed(int buttonId) {
+        if (IDialogConstants.OK_ID == buttonId) {
+            connectionFactory.save(connName, prefStore);
+        }
+        super.buttonPressed(buttonId);
+    }
+    
+    @Override
+    protected void configureShell(Shell newShell) {
+        super.configureShell(newShell);
+        newShell.setText("Configure the connection");
+    }
+    
+    @Override
+    protected Point getInitialSize() {
+        return new Point(300, 400);
+    }
+    
+}
