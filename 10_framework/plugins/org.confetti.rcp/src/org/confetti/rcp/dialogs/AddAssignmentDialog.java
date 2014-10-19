f51aeb9c0ca8624e543e653fe4e824160f531a47
bublag <gaabor6@gmail.com>
2014-10-19 21:29:05 +0200

    Possible to create new assignments from the UI
    AddAssignmentDialog handles the creation of new assignments

--- 10_framework/plugins/org.confetti.rcp/src/org/confetti/rcp/dialogs/AddAssignmentDialog.java
+++ 10_framework/plugins/org.confetti.rcp/src/org/confetti/rcp/dialogs/AddAssignmentDialog.java
@@ -1,18 +1,40 @@
 package org.confetti.rcp.dialogs;
 
+import static com.google.common.collect.Iterables.isEmpty;
+import static com.google.common.collect.Iterables.toArray;
+
+import java.util.List;
+
 import org.confetti.core.Nameable;
+import org.confetti.core.StudentGroup;
+import org.confetti.core.Subject;
+import org.confetti.core.Teacher;
 import org.confetti.rcp.ConfettiPlugin;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.resource.FontDescriptor;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * @author Gabor Bubla
@@ -39,19 +61,61 @@ public class AddAssignmentDialog extends Dialog {
         newShell.setText("Add assignment");
     }
 
+    //create only the OK button and don't create Cancel
+    @Override
+    protected void createButtonsForButtonBar(Composite parent) {
+        super.createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+    }
+
     @Override
     protected Control createDialogArea(Composite parent) {
         Composite container = (Composite) super.createDialogArea(parent);
+        container.setLayout(new GridLayout(3, true));
         
-        ListViewer subjectsViewer = new ListViewer(container, SWT.BORDER | SWT.SINGLE);
+        //create labels
+        Label informationLabel = new Label(container, SWT.NONE);
+        informationLabel.setText(
+                "Select a subject, one or more teacher and one or more student group. " + 
+                "(Use the Ctrl and Shift keys for multiple selection)");
+        GridDataFactory.fillDefaults().span(3, 1).applyTo(informationLabel);
+        
+        //create bold fonts for these 3 labels
+        Label subjectLabel = new Label(container, SWT.NONE);
+        FontDescriptor boldDescriptor = FontDescriptor.createFrom(subjectLabel.getFont()).setStyle(SWT.BOLD);
+        Font boldFont = boldDescriptor.createFont(subjectLabel.getDisplay());
+        subjectLabel.setFont(boldFont);
+        subjectLabel.setText("Subjects");
+        Label teacherLabel = new Label(container, SWT.NONE);
+        teacherLabel.setText("Teachers");
+        teacherLabel.setFont(boldFont);
+        Label studentGroupLabel = new Label(container, SWT.NONE);
+        studentGroupLabel.setText("Student groups");
+        studentGroupLabel.setFont(boldFont);
+        
+        //create viewers
+        ListViewer subjectsViewer = new ListViewer(container, SWT.BORDER | SWT.SINGLE | SWT.V_SCROLL);
         subjectsViewer.setContentProvider(new ArrayContentProvider());
         subjectsViewer.setLabelProvider(new EntityLabelProvider());
         subjectsViewer.setInput(ConfettiPlugin.getDefault().getDataProvider().getValue().getSubjects().getList());
-
+        GridDataFactory.fillDefaults().grab(true, true).applyTo(subjectsViewer.getControl());
         
+        ListViewer teachersViewer = new ListViewer(container, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+        teachersViewer.setContentProvider(new ArrayContentProvider());
+        teachersViewer.setLabelProvider(new EntityLabelProvider());
+        teachersViewer.setInput(ConfettiPlugin.getDefault().getDataProvider().getValue().getTeachers().getList());
+        GridDataFactory.fillDefaults().grab(true, true).applyTo(teachersViewer.getControl());
         
-        Button addButton = new Button(container, SWT.NONE);
-        addButton.setText("Create assignment");
+        TreeViewer studentgroupsViewer = new TreeViewer(container, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.FULL_SELECTION);
+        studentgroupsViewer.setContentProvider(new StudentGroupContentProvider());
+        studentgroupsViewer.setLabelProvider(new EntityLabelProvider());
+        studentgroupsViewer.setInput(ConfettiPlugin.getDefault().getDataProvider().getValue().getStudentGroups().getList());
+        GridDataFactory.fillDefaults().grab(true, true).applyTo(studentgroupsViewer.getControl());
+        
+        //create button
+        Button createButton = new Button(container, SWT.NONE);
+        createButton.setText("Create assignment");
+        createButton.addSelectionListener(new CreateAssignmentSelectionListener(subjectsViewer, teachersViewer, studentgroupsViewer));
+        
         return container;
     }
     
@@ -63,6 +127,58 @@ public class AddAssignmentDialog extends Dialog {
                 return ((Nameable) element).getName().getValue();
             } 
             return null;
+        }
+    }
+    
+    private class StudentGroupContentProvider implements ITreeContentProvider {
+        @Override public void dispose() { }
+        @Override public void inputChanged(Viewer viewer, Object oldInput, Object newInput) { }
+        @Override public Object[] getElements(Object parent)    { return ((List<?>) parent).toArray(); }
+        @Override public Object getParent(Object child)         { return ((StudentGroup) child).getParent(); }
+        @Override public boolean hasChildren(Object parent)     { return !isEmpty(((StudentGroup) parent).getChildren().getList()); }
+        @Override public Object[] getChildren(Object parent)    { return toArray(((StudentGroup) parent).getChildren().getList(), StudentGroup.class); }
+    }
+    
+    private class CreateAssignmentSelectionListener extends SelectionAdapter {
+
+        private ListViewer subjectsViewer;
+        private ListViewer teachersViewer;
+        private TreeViewer studentgroupsViewer;
+
+        public CreateAssignmentSelectionListener(ListViewer subjectsViewer, ListViewer teachersViewer, TreeViewer studentgroupsViewer) {
+            this.subjectsViewer = subjectsViewer;
+            this.teachersViewer = teachersViewer;
+            this.studentgroupsViewer = studentgroupsViewer;
+        }
+        
+        @Override
+        public void widgetSelected(SelectionEvent e) {
+            Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+            
+            //warning dialog if a viewer has no selection
+            if (subjectsViewer.getSelection().isEmpty() || teachersViewer.getSelection().isEmpty() || studentgroupsViewer.getSelection().isEmpty()) {
+                MessageDialog.openWarning(shell, 
+                        "Wrong selection", "Please select a subject, one or more teacher and one or more student group.");
+                return;
+            }
+            
+            //collect the selections
+            IStructuredSelection selection = (IStructuredSelection) subjectsViewer.getSelection();
+            Subject subject = (Subject) selection.getFirstElement();
+            selection = (IStructuredSelection) teachersViewer.getSelection();
+            List<Teacher> teachers = selection.toList();
+            selection = (IStructuredSelection) studentgroupsViewer.getSelection();
+            List<StudentGroup> studentGroups = selection.toList();
+            
+            //create the assignment
+            //FIXME add assignment only if it doesn't exist already
+            ConfettiPlugin.getDefault().getDataProvider().getValue().addAssignment(subject, teachers, studentGroups);
+            MessageDialog.openInformation(shell, "Success", "Selected assignment succesfully added");
+            
+            //reset viewers' selection
+            subjectsViewer.setSelection(null);
+            teachersViewer.setSelection(null);
+            studentgroupsViewer.setSelection(null);
         }
     }
     
