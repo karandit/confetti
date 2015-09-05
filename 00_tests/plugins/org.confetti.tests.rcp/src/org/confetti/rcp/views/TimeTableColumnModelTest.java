cfda0efce68ef3786c900eaab78f33ddc2f23b4c
karandit <tkarandi@gmail.com>
2015-09-05 16:15:45 +0200

    Span the top left corner for column based timetables

--- 00_tests/plugins/org.confetti.tests.rcp/src/org/confetti/rcp/views/TimeTableColumnModelTest.java
+++ 00_tests/plugins/org.confetti.tests.rcp/src/org/confetti/rcp/views/TimeTableColumnModelTest.java
@@ -1,39 +1,85 @@
 package org.confetti.rcp.views;
 
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.confetti.core.DataProvider;
 import org.confetti.core.StudentGroup;
 import org.confetti.observable.ListMutator;
+import org.confetti.observable.ValueMutator;
+import org.eclipse.swt.graphics.Point;
 import org.junit.Before;
 import org.junit.Test;
+
+import de.kupzog.ktable.renderers.FixedCellRenderer;
 
 public class TimeTableColumnModelTest  {
 	
 	private TimeTableColumnModel sut;
 	
+	/**
+	 * +----------+--------------------+--------------------+
+	 * |          |         AA         |         AB         |
+	 * |          +---------+----------+---------+----------+
+	 * |          |  AA1    |   AA2    |    AB1  |   AB2    |
+	 * +-----+----+---------+----------+---------+----------+
+	 * |  M  | 08 |                                         | 
+	 * |  O  +----+-----------------------------------------+ 
+	 * |  N  | 09 |                                         |
+	 * |  D  +----+-----------------------------------------+
+	 * |  A  | 10 |                                         |
+	 * |  Y  +----+-----------------------------------------+
+	 * |     | 11 |                                         |
+	 * |     +----+-----------------------------------------+
+	 * |     | 12 |                                         |
+	 * |     +----+-----------------------------------------+
+	 * |     | 13 |                                         |
+	 * +-----+----+-----------------------------------------+
+	 * |  T  | 08 |                                         | 
+	 * |  U  +----+-----------------------------------------+ 
+	 * |  E  | 09 |                                         |
+	 * |  S  +----+-----------------------------------------+
+	 * |  D  | 10 |                                         |
+	 * |  A  +----+-----------------------------------------+
+	 * |  Y  | 11 |                                         |
+	 * |     +----+-----------------------------------------+
+	 * |     | 12 |                                         |
+	 * |     +----+-----------------------------------------+
+	 * |     | 13 |                                         |
+	 * +-----+----+-----------------------------------------+
+	 * 
+	 * 
+	 */
 	@Before
 	public void setUp() {
 		DataProvider dp = mock(DataProvider.class);
 		when(dp.getDays()).thenReturn(null);
-		StudentGroup sg = mock(StudentGroup.class);
-		when(sg.getChildren()).thenReturn(new ListMutator<StudentGroup>().getObservableList());
-		
-		
+		StudentGroup sg = mockStudentGroup("A", 
+							mockStudentGroup("AA",
+								mockStudentGroup("AA1"),
+								mockStudentGroup("AA2")
+							),
+							mockStudentGroup("AB",
+								mockStudentGroup("AB1"),
+								mockStudentGroup("AB2")
+							)
+		);
 		sut = new TimeTableColumnModel(null, dp, sg);
 		sut.initialize();
 	}
 	
 	@Test public void testGetFixedHeaderColumnCount() 		{ assertEquals(2, sut.getFixedHeaderColumnCount()); }
-	@Test public void testGetFixedHeaderRowCount() 			{ assertEquals(1, sut.getFixedHeaderRowCount()); }
+	@Test public void testGetFixedHeaderRowCount() 			{ assertEquals(2, sut.getFixedHeaderRowCount()); }
 	@Test public void testGetFixedSelectableColumnCount() 	{ assertEquals(0, sut.getFixedSelectableColumnCount()); }
 	@Test public void testGetFixedSelectableRowCount() 		{ assertEquals(0, sut.getFixedSelectableRowCount()); }
 
-	@Test public void testDoGetRowCount() 					{ assertEquals(1, sut.doGetRowCount()); }
-	@Test public void testDoGetColumnCount() 				{ assertEquals(3, sut.doGetColumnCount()); }
+	@Test public void testDoGetRowCount() 					{ assertEquals(2, sut.doGetRowCount()); }
+	@Test public void testDoGetColumnCount() 				{ assertEquals(6, sut.doGetColumnCount()); }
 	
 	@Test public void testGetInitialColumnWidth() 			{ assertEquals(60, sut.getInitialColumnWidth(0)); } 
 	@Test public void testGetInitialRowHeight() 			{ assertEquals(48, sut.getInitialRowHeight(0)); } 
@@ -42,10 +88,44 @@ public class TimeTableColumnModelTest  {
 	@Test public void testIsColumnResizable() 				{ assertFalse(sut.isColumnResizable(0)); } 
 	@Test public void testIsRowResizable() 					{ assertFalse(sut.isRowResizable(0)); } 
 	
-	@Test public void testDoGetCellEditorIntInt() 			{ assertEquals(0, sut.getFixedSelectableRowCount()); } 
-	@Test public void testDoGetCellRendererIntInt() 		{ assertEquals(0, sut.getFixedSelectableRowCount()); } 
+	@Test public void testDoGetCellEditor() 				{ assertNull(sut.getCellEditor(0, 0)); } 
+
+	@Test
+	public void testBelongToCell_TopLeft_Blank_Corner() {
+		assertPoint(0, 0, sut.belongsToCell(0, 0));
+		assertPoint(0, 0, sut.belongsToCell(1, 0));
+		assertPoint(0, 0, sut.belongsToCell(0, 1));
+		assertPoint(0, 0, sut.belongsToCell(1, 1));
+	}
+
+	@Test
+	public void testBelongToCell_StudentGroup_Names() {
+		assertPoint(2, 0, sut.belongsToCell(2, 0));
+//		assertPoint(2, 0, sut.belongsToCell(3, 0));
+		assertPoint(4, 0, sut.belongsToCell(4, 0));
+//		assertPoint(4, 0, sut.belongsToCell(5, 0));
+		assertPoint(2, 1, sut.belongsToCell(2, 1));
+		assertPoint(3, 1, sut.belongsToCell(3, 1));
+		assertPoint(4, 1, sut.belongsToCell(4, 1));
+		assertPoint(5, 1, sut.belongsToCell(5, 1));
+	}
+	
+	@Test
+	public void testDoGetCellRenderer() {
+		assertTrue(sut.getCellRenderer(0, 0) instanceof FixedCellRenderer);
+	}
 
 //	@Test public void testDoGetContentAt() 					{ assertEquals(CONTENT, sut.getContentAt(0, 0)); }
-
+	//----------------------- helpers ----------------------------------------------------------------------------------
+	private StudentGroup mockStudentGroup(String name, StudentGroup...children) {
+		StudentGroup sg = mock(StudentGroup.class);
+		when(sg.getName()).thenReturn(new ValueMutator<>(this, name).getObservableValue());
+		when(sg.getChildren()).thenReturn(new ListMutator<>(asList(children)).getObservableList());
+		return sg;
+	}
+	
+	private void assertPoint(int x, int y, Point actualPoint) {
+		assertEquals(new Point(x, y), actualPoint);
+	}
 
 }
--- 10_framework/plugins/org.confetti.rcp/src/org/confetti/rcp/views/TimeTableColumnModel.java
+++ 10_framework/plugins/org.confetti.rcp/src/org/confetti/rcp/views/TimeTableColumnModel.java
@@ -4,40 +4,43 @@ import static de.kupzog.ktable.renderers.DefaultCellRenderer.STYLE_PUSH;
 
 import org.confetti.core.DataProvider;
 import org.confetti.core.StudentGroup;
+import org.eclipse.swt.graphics.Point;
 
 import de.kupzog.ktable.KTable;
 import de.kupzog.ktable.KTableCellEditor;
 import de.kupzog.ktable.KTableCellRenderer;
 import de.kupzog.ktable.KTableNoScrollModel;
-import de.kupzog.ktable.renderers.DefaultCellRenderer;
 import de.kupzog.ktable.renderers.FixedCellRenderer;
 
 public class TimeTableColumnModel extends KTableNoScrollModel {
 
 	//----------------------------- constants --------------------------------------------------------------------------
-	private static final KTableCellRenderer RENDERER = new DefaultCellRenderer(STYLE_PUSH);
+//	private static final KTableCellRenderer RENDERER = new DefaultCellRenderer(STYLE_PUSH);
 	private static final FixedCellRenderer FIXED_RENDERER = new FixedCellRenderer(STYLE_PUSH);
 	
 	//----------------------------- fields -----------------------------------------------------------------------------
 //	private final DataProvider dp;
-	private final StudentGroup sg;
-
+//	private final StudentGroup sg;
+	private final int namesWidth;
+	private final int namesHeight;
+	
 	//----------------------------- constructors -----------------------------------------------------------------------
 	public TimeTableColumnModel(KTable table, DataProvider dp, StudentGroup sg) {
 		super(table);
 //		this.dp = dp;
-		this.sg = sg;
+//		this.sg = sg;
+		this.namesWidth = calcWidth(sg);
+		this.namesHeight = calcHeight(sg) - 1;
 	}
 	
 	//----------------------------- KTableNoScrollModel's API ----------------------------------------------------------
 	@Override public int getFixedHeaderColumnCount() 							{ return 2; }
-	@Override public int getFixedHeaderRowCount() 								{ return 1; }
+	@Override public int getFixedHeaderRowCount() 								{ return namesHeight; }
 	@Override public int getFixedSelectableColumnCount() 						{ return 0; }
 	@Override public int getFixedSelectableRowCount() 							{ return 0; }
 
-	@Override public int doGetColumnCount() 									{ return 2 + dagWidth(sg);}
-	@Override public int doGetRowCount() 										{ return dagDepth(sg); }
-	// + size(dp.getDays().getList()) * size(dp.getHours().getList());
+	@Override public int doGetColumnCount() 									{ return 2 + namesWidth;}
+	@Override public int doGetRowCount() 										{ return namesHeight; }
 	
 	@Override public int getInitialColumnWidth(int col) 						{ return 60; }
 	@Override public int getInitialRowHeight(int row) 							{ return 48; }
@@ -47,24 +50,38 @@ public class TimeTableColumnModel extends KTableNoScrollModel {
 	@Override public boolean isRowResizable(int row) 							{ return false; }
 
 	@Override public KTableCellEditor doGetCellEditor(int arg0, int arg1) 		{ return null; }
-	@Override public KTableCellRenderer doGetCellRenderer(int col, int row) 	{ return (row == 0 || col == 1 || col == 0) ? FIXED_RENDERER : RENDERER; }
+	@Override public KTableCellRenderer doGetCellRenderer(int col, int row) 	{ return FIXED_RENDERER; }
 	
 	@Override public void doSetContentAt(int arg0, int arg1, Object arg2) 		{ }
-	@Override public Object doGetContentAt(int col, int row) 					{ return "alam"; }
+	@Override public Object doGetContentAt(int col, int row) 					{ return ""; }
 
+	@Override
+	public Point belongsToCell(int col, int row) {
+		//top left blank corner
+		if (col < getFixedHeaderColumnCount() && row < getFixedHeaderRowCount()) {
+			return new Point(0, 0);
+		}
+		//student group names
+//		if (row < getFixedHeaderRowCount()) {
+//			return new Point(0, 0);
+//		}
+		
+		return super.belongsToCell(col, row);
+	}
+	
 	//----------------------------- helpers ----------------------------------------------------------------------------
-	private static int dagWidth(StudentGroup sg) {
+	private static int calcWidth(StudentGroup sg) {
 		int childWidth = 0;
 		for (StudentGroup child : sg.getChildren().getList()) {
-			childWidth += dagWidth(child);
+			childWidth += calcWidth(child);
 		}
 		return Math.max(1, childWidth);
 	}
 
-	private static int dagDepth(StudentGroup sg) {
+	private static int calcHeight(StudentGroup sg) {
 		int childDepth = 0;
 		for (StudentGroup child : sg.getChildren().getList()) {
-			childDepth = Math.max(childDepth, dagDepth(child));
+			childDepth = Math.max(childDepth, calcHeight(child));
 		}
 		return 1 + childDepth;
 	}
